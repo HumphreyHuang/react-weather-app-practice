03210711e379194cca63e1936a96bf8edd448c4a
Humphrey <humphreyhzd@gmail.com>
2017-11-13 20:46:43 +0000

    Added Basic Table Rendering

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,15 @@
 import axios from 'axios';
 
 const API_KEY = '6f5017299e1d575eb6d5f002eef64899';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${
+    API_KEY
+}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
     const url = `${ROOT_URL}&q=${city},us`;
     const request = axios.get(url);
-
-    console.log('Request', request);
 
     return {
         type: FETCH_WEATHER,
